45272c5cd976dcec8772c09bff267c672240f749
ashok siva <ashoksiva0906@gmail.com>
2025-03-17 21:52:12 +0530

    Updated

--- src/test/java/org/runner/Run.java
+++ src/test/java/org/runner/Run.java
@@ -1,13 +1,12 @@
 package org.runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
-        features = "src/test/resources",
-        glue = {"java/org/steps"},
+        features = "C:\\Users\\Ashok\\IdeaProjects\\BDDCucumberframework\\src\\test\\resources\\features",
+        glue = {"org.steps"},
         tags = "@Regression",
         plugin = { "pretty", "html:target/cucumber-reports" }
 )
-public class Run extends AbstractTestNGCucumberTests {
+public class Run extends AbstractTestNGCucumberTests{
 }
