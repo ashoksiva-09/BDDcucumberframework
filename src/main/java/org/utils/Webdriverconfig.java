d15b4885c2859e71eff42c53dc485a617f54c027
ashok siva <ashoksiva0906@gmail.com>
2026-07-06 15:28:15 +0530

    Update

--- src/main/java/org/utils/Webdriverconfig.java
+++ src/main/java/org/utils/Webdriverconfig.java
@@ -3,6 +3,7 @@ package org.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,8 +11,10 @@ public class Webdriverconfig {
     protected static WebDriver driver;
     public static WebDriver driver() {
 //        System.setProperty("webdriver.chrome.driver", "..//sampleAPI//target//chromedriver.exe");
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--incognito");
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(option);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
--- src/test/java/org/runner/Run.java
+++ src/test/java/org/runner/Run.java
@@ -3,7 +3,7 @@ package org.runner;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
-        features = "C:\\Users\\Ashok\\IdeaProjects\\BDDCucumberframework\\src\\test\\resources\\features",
+        features = "./src/test/resources/features",
         glue = {"org.steps"},
         tags = "@Regression",
         plugin = { "pretty", "html:target/cucumber-reports" }
